fa95ef9716fdc5892e9d977cc4a6433b76fb8965
Raghuveer Sharma <sharmaraghu157@gmail.com>
2024-12-21 12:57:22 +0530

    initial commit

---
+++ array/FrequencyOfElement.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Sort(vector<int> &v)
+{
+    int n = v.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (v[j - 1] > v[j])
+            {
+                swap(v[j - 1], v[j]);
+            }
+        }
+    }
+}
+
+void frequency(vector<int> &v)
+{
+    int n = v.size(), count = 0, element = v[0];
+    for (int i = 0; i <= n; i++)
+    {
+        if (element == v[i])
+        {
+            count++;
+        }
+        else
+        {
+            cout << element << ":" << count << " ";
+            element = v[i];
+            count = 1;
+        }
+    }
+}
+int main()
+{
+    vector<int> v = {4, 1, 4, 2, 1, 5, 6, 4};
+    int n = v.size();
+    Sort(v);
+    frequency(v);
+    cout << endl;
+
+    for (int num : v)
+    {
+        cout << num;
+    }
+
+    return 0;
+}
